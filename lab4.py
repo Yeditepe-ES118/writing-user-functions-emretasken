deda73381ff64920aa3cceda20ae88941d325af8
emretasken <emre.tasken@std.yeditepe.edu.tr>
2025-10-22 19:01:24 +0300

    Update lab4.py

--- lab4.py
+++ lab4.py
@@ -7,7 +7,7 @@ def throw_rock(m,v0,theta):
     hm = v0 ** 2 * np.sin(theta) ** 2 / (2*g) # in m
     vh = v0 * np.cos(theta) # in m/s
     Kh = 0.5 *m * vh * hm ** 2 # in J
-    print("For a rock with %5.3f kg mass thrown with %5.3f m/s at an angle of %6.2f degreees:\n"\
+    print("For a rock with %5.3f kg mass thrown with %5.3f m/s at an angle of %6.2f degrees:\n"\
           "Time of flight is %10.1e s\nThe range in x-direction is %10.1e m\n"\
              "Maximum height is %10.1e m\nThe speed at maximum height is %10.1e m/s\n"\
                  "Kinetic energy at the maximum height is %8.2e J" % (m,v0,theta * 180 / np.pi,tf,R,hm,vh,Kh))
